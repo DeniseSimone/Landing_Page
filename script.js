75e47fa8fd63e73ff3d17d5e9e157cf3eac914b5
denise custodio <denisesimone1997@gmail.com>
2022-02-01 13:23:48 +0000

    changes

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  alert(
+    "This website was made for computer screens. At moment moment we're working on making it more responsive for other electrical devices."
+  );
+}
